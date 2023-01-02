6dcae9828b8077a9bcc917c86d57cc6e3dba68e9
WongTzeKin <tzekinwong@icloud.com>
2023-01-02 20:15:26 +0800

    Initial Commit

---
+++ Gymetrics/GymetricsApp.swift
@@ -0,0 +1,17 @@
+//
+//  GymetricsApp.swift
+//  Gymetrics
+//
+//  Created by 黃子健 on 2/1/2023.
+//
+
+import SwiftUI
+
+@main
+struct GymetricsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
